167a9991daeb6d54a9c79ff87fdbfb2d84f69104
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-09-16 09:38:27 -0600

    changed migration names And created target areas seed

--- seeds/00_targetArea.js
+++ seeds/00_targetArea.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('shapes').insert([
-        {id: 1, name: 'rowValue1', target_id: '', description: '', image_URL: '' },
-        {id: 2, name: 'rowValue2', target_id: '', description: '', image_URL: ''},
-        {id: 3, name: 'rowValue3', target_id: '', description: '', image_URL: ''},
+        {id: 1, targetArea: 'Upper Body' },
+        {id: 2, TargetArea: 'Lower Body' },
+        {id: 3, TargetArea: 'Abdominals' },
       ])
     })
 }
