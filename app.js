234e18b36b79d7aaa72ff463a3243ddd8525ab9f
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-09-16 09:05:31 -0600

    configured pg

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const app = express()
+const queries = require('./queries')
+
+app.use(cors())
+app.use(bodyParser.json())
--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,14 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/shapes',
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  
+  
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+  },
+}
